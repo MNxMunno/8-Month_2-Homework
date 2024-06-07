caec2258aff2cab9d87be62ab7b524110d2e725e
MNxMunno <bilolhuzayfa@gmail.com>
2024-06-08 01:38:44 +0500

    change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Header = () => {
+  const [toggel, setToggel] = useState(false);
   return (
     <header>
       <div className="container">
@@ -8,12 +10,15 @@ const Header = () => {
           <a href="#" className="logo">
             Portfolio
           </a>
-          <div className="nav__items">
+          <div className={`nav__items ${toggel ? "show" : ""}`}>
             <a href="#">Home</a>
             <a href="#">About</a>
             <a href="#">Blog</a>
-            <a href="#">Contact</a>
+            <a href="#contact">Contact</a>
           </div>
+          <button className="menu" onClick={() => setToggel(!toggel)}>
+            <BiMenuAltRight />
+          </button>
         </nav>
       </div>
     </header>
